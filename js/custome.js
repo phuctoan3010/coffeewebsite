bf2a777754a68d977ca5bf2da70b77a96eb532bb
phuctoan3010 <143265079+phuctoan3010@users.noreply.github.com>
2023-08-26 19:15:06 +0700

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,92 @@
+// JavaScript Document$('#banner').owlCarousel({
+
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+//    nav:true,
+//    center:true,
+	dots:true,
+	autoplay:true,
+	animateOut:'fadeOut',
+	animateIn: 'fadein',
+	autoplayTimeout:3000,
+	autoplaySpeed:1500,
+	dotsSpeed: 1500,
+	autoplayHoverPause:true,
+    smartSpeed:1000,
+//    stagePadding:50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#banner2').owlCarousel({
+    loop:true,
+    margin:10,
+//    nav:true,
+//    center:true,
+	dots:true,
+	autoplay:true,
+	animateOut:'fadeOut',
+	animateIn: 'fadein',
+	autoplayTimeout:3000,
+	autoplaySpeed:1500,
+	dotsSpeed: 1500,
+	autoplayHoverPause:true,
+    smartSpeed:1000,
+//    stagePadding:50,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+
+//$('#banner2').owlCarousel({
+//    loop:true,
+//    margin:10,
+//    responsiveClass:true,
+//    autoplay:true,
+//    animateOut:'fadeOut',
+//	animateIn: 'fadein',
+//	autoplayTimeout:3000,
+//	autoplaySpeed:1500,
+//	dotsSpeed: 1500,
+//	autoplayHoverPause:true,
+//    smartSpeed:1000,
+//    responsive:{
+//        0:{
+//            items:1,
+//            nav:true
+//        },
+//        600:{
+//            items:2,
+//            nav:false
+//        },
+//        1000:{
+//            items:2,
+//            nav:true,
+//            loop:false
+//        }
+//    }
+//})
+
+
+
+
